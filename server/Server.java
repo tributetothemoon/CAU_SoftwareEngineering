b459e0901b4de574cdbbdb127caaad56d03afc76
kkm97351@gmail.com <kkm97351@gmail.com>
2020-06-21 05:33:43 +0900

    small modification

--- server/Server.java
+++ server/Server.java
@@ -1,12 +1,12 @@
 package server;
+import java.util.ArrayList;
 import server.bookdatabase.*;
 import server.userdatabase.*;
-import java.util.ArrayList;
 
 public class Server {
 	public static void initialize() {UserDatabase.initialize();BookDatabase.initialize();}
 	
-	//UserDatabase access
+	////////////////////////////User Data Function////////////////////////////////
 	//get user information
 	public static String get_id(Object account) {return UserDatabase.get_id((User)account);}
 	public static String get_name(Object account) {return UserDatabase.get_name((User)account);}
@@ -16,11 +16,13 @@ public class Server {
 	public static boolean is_admin(Object account) {return UserDatabase.is_admin((User)account);}
 	public static boolean activate(Object account) {return UserDatabase.activate((User)account);}
 	public static boolean deactivate(Object account) {return UserDatabase.deactivate((User)account);}
+	public static ArrayList<Object> query_all_user(){return UserDatabase.query_all_user();}
+	
 	public static boolean delete_user(Object account) {
 		if(is_activated(account)) return false;	//account is activated. Cannot delete
 		ArrayList<Object> bookList = query_book(account);
-		if(bookList != null)	for(Object cur : bookList) delete_book(cur);	//
-		return UserDatabase.delete_user((User)account);}
+		if(bookList != null)	for(Object cur : bookList) BookDatabase.delete_book((Book)cur);	//
+		return UserDatabase.delete_user((User)account);}	//true or false
 	
 	public static Object login(String id, String password) throws Exception {return UserDatabase.login(id, password);}
 	
@@ -40,10 +42,8 @@ public class Server {
 			return email;
 		}
 	}
-	
-	public static ArrayList<Object> query_all_user(){return UserDatabase.query_all_user();}
 
-	//BookDatabase access
+	////////////////////////////////Book Data Function/////////////////////////////////
 	
 	//get book information
 	public static int get_ISBN(Object book) {return BookDatabase.get_ISBN((Book)book);}	
@@ -53,14 +53,12 @@ public class Server {
 	public static int get_published_year(Object book) {return BookDatabase.get_published_year((Book)book);}	
 	public static int get_price(Object book) {return BookDatabase.get_price((Book)book);}	
 	public static String get_condition(Object book) {return BookDatabase.get_condition((Book)book);}	
-	public static String get_seller_id(Object book) {
-		String seller = BookDatabase.get_seller_id((Book)book);
-		return seller;
-	}
+	public static String get_seller_id(Object book) {return BookDatabase.get_seller_id((Book)book);}
+	public static void delete_book(Object book) {BookDatabase.delete_book((Book)book);}
 
 	public static void upload_book(String title, int ISBN, String author, String publisher, 
 			int publication_year, int price, String state, Object seller) {
-		BookDatabase.upload_book(title, ISBN, author, publisher, publication_year, price, state, get_id((User)seller));
+		BookDatabase.upload_book(title, ISBN, author, publisher, publication_year, price, state, UserDatabase.get_id((User)seller));
 	}
 	
 	public static ArrayList<Object> search_book(String title, int ISBN, String author, String seller_id) {
@@ -77,6 +75,6 @@ public class Server {
 			int published_year, int price, String condition) {
 		BookDatabase.modify_book((Book)book, title, ISBN, author, publisher, published_year, price, condition);
 	}
-	public static void delete_book(Object book) {BookDatabase.delete_book((Book)book);}
+	
 	
 }
